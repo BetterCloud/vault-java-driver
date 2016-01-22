e82940d0a4fe7780b139ff88acd17d4469cf99e0
Steve Perkins <steve@steveperkins.com>
2016-01-21 20:11:04 -0500

    Updates to retry testing

--- src/test/java/com/bettercloud/vault/VaultTests.java
+++ src/test/java/com/bettercloud/vault/VaultTests.java
@@ -15,18 +15,26 @@ public class VaultTests {
     @Test
     public void testRetries() throws Exception {
         final Server server = new Server(8999);
-        server.setHandler(new MockVault());
+        server.setHandler(new MockVault(5));
         server.start();
 
         final VaultConfig vaultConfig = new VaultConfig("http://127.0.0.1:8999", "...");
+        vaultConfig.retryOption(VaultException.class, 5, 1);
         final Vault vault = new Vault(vaultConfig);
-        vault.logical().write("secret/hello", "world");
+        vault.logical().read("secret/hello");
         System.out.println("done");
     }
 
 }
 
 class MockVault extends AbstractHandler {
+
+    private int failureCount;
+
+    public MockVault(final int failureCount) {
+        this.failureCount = failureCount;
+    }
+
     @Override
     public void handle(
             final String target,
@@ -35,8 +43,15 @@ class MockVault extends AbstractHandler {
             final HttpServletResponse response
     ) throws IOException, ServletException {
         response.setContentType("application/json");
-        response.setStatus(200);
         baseRequest.setHandled(true);
-        response.getWriter().println("{}");
+        if (failureCount > 0) {
+            failureCount = failureCount - 1;
+            response.setStatus(500);
+            System.out.printf("Sending 500... failureCount == %d\n", failureCount);
+        } else {
+            response.setStatus(200);
+            response.getWriter().println("{}");
+        }
     }
 }
+
