44daa5550cd8f84a21946188a84b9deddfb6fef1
Steve Perkins <steve@steveperkins.com>
2016-04-28 13:58:35 -0400

    Implements token self-renewal

--- src/test-integration/java/com/bettercloud/vault/api/AuthTests.java
+++ src/test-integration/java/com/bettercloud/vault/api/AuthTests.java
@@ -3,8 +3,13 @@ package com.bettercloud.vault.api;
 import com.bettercloud.vault.Vault;
 import com.bettercloud.vault.VaultConfig;
 import com.bettercloud.vault.VaultException;
+import com.bettercloud.vault.json.Json;
+import com.bettercloud.vault.response.AuthResponse;
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
+
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
 
@@ -65,6 +70,47 @@ public class AuthTests {
         final String token = vault.auth().loginByUsernamePassword(path, password).getAuthClientToken();
         assertNotNull(token);
         assertNotSame("", token.trim());
+    }
+
+    /**
+     * Tests token self-renewal for the token auth backend.
+     *
+     * @throws VaultException
+     */
+    @Test
+    public void testRenewSelf() throws VaultException, UnsupportedEncodingException {
+        // Check environment variables
+        final String address = System.getProperty("VAULT_ADDR");
+        final String userId = System.getProperty("VAULT_USER_ID");
+        final String password = System.getProperty("VAULT_PASSWORD");
+        assertNotNull(address);
+        assertNotNull(userId);
+        assertNotNull(password);
+
+        // Get an initial auth token
+        final VaultConfig authConfig = new VaultConfig(address);
+        final Vault authVault = new Vault(authConfig);
+        final AuthResponse authResponse = authVault.auth().loginByUsernamePassword("userpass/login/" + userId, password);
+        final String authToken = authResponse.getAuthClientToken();
+        assertNotNull(authToken);
+        assertNotSame("", authToken.trim());
+
+        // Renew the auth token
+        final VaultConfig renewConfig = new VaultConfig(address, authToken);
+        final Vault renewVault = new Vault(renewConfig);
+        final AuthResponse renewResponse = renewVault.auth().renewSelf();
+        final String renewToken = renewResponse.getAuthClientToken();
+        assertEquals(authToken, renewToken);
+
+        // Renew the auth token, with an explicit increment value
+        final VaultConfig explicitConfig = new VaultConfig(address, authToken);
+        final Vault explicitVault = new Vault(explicitConfig);
+        final AuthResponse explicitResponse = explicitVault.auth().renewSelf(20);
+        final String explicitToken = explicitResponse.getAuthClientToken();
+        assertEquals(authToken, explicitToken);
+        final String explicitJson = new String(explicitResponse.getRestResponse().getBody(), "UTF-8");
+        final long explicitLeaseDuration = Json.parse(explicitJson).asObject().get("auth").asObject().get("lease_duration").asLong();
+        assertEquals(20, explicitLeaseDuration);
     }
 
 }
