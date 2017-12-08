ff7e159be0625b923d1b62ca3e17b81996fb2cef
Dan Harris <daniel.harris@asurion.com>
2017-12-08 11:06:53 -0500

    initial implementation with tests

---
+++ src/test-integration/java/com/bettercloud/vault/api/AuthBackendAwsTests.java
@@ -0,0 +1,122 @@
+package com.bettercloud.vault.api;
+
+import com.bettercloud.vault.Vault;
+import com.bettercloud.vault.VaultConfig;
+import com.bettercloud.vault.VaultException;
+import com.bettercloud.vault.json.Json;
+import com.bettercloud.vault.json.JsonObject;
+import com.bettercloud.vault.response.AuthResponse;
+import com.bettercloud.vault.util.VaultContainer;
+import com.bettercloud.vault.vault.VaultTestUtils;
+import com.bettercloud.vault.vault.mock.AuthRequesValidatingMockVault;
+import org.apache.commons.io.IOUtils;
+import org.eclipse.jetty.server.Server;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.shaded.com.google.common.collect.ImmutableCollection;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.function.Predicate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+
+public class AuthBackendAwsTests {
+
+    private JsonObject readRequestBody(HttpServletRequest request) {
+        try {
+            StringBuilder requestBuffer = new StringBuilder();
+            IOUtils.readLines(request.getReader()).forEach(requestBuffer::append);
+            return Json.parse(requestBuffer.toString()).asObject();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Test
+    public void testLoginByAwsEc2() throws Exception {
+        final Predicate<HttpServletRequest> isValidEc2pkcs7Request = (request) -> {
+            JsonObject requestBody = readRequestBody(request);
+            return requestBody != null && request.getRequestURI().endsWith("/auth/aws/login") &&
+                  requestBody.getString("pkcs7", "") == "pkcs7";
+        };
+
+        final Predicate<HttpServletRequest> isValidEc2IdRequest = (request) -> {
+            JsonObject requestBody = readRequestBody(request);
+            return requestBody != null && request.getRequestURI().endsWith("/auth/aws/login") &&
+                    requestBody.getString("identity", "") == "identity" &&
+                    requestBody.getString("signature", "") == "signature";
+        };
+
+        final Predicate<HttpServletRequest> isValidEc2IamRequest = (request) -> {
+            JsonObject requestBody = readRequestBody(request);
+            return requestBody != null && request.getRequestURI().endsWith("/auth/aws/login") &&
+                    requestBody.getString("iam_http_request_method", "") == "POST" &&
+                    requestBody.getString("iam_http_request_url", "") == "url" &&
+                    requestBody.getString("iam_http_request_body", "") == "body" &&
+                    requestBody.getString("iam_http_request_headers", "") == "headers";
+        };
+
+        final AuthRequesValidatingMockVault mockVault =  new AuthRequesValidatingMockVault(new HashSet<Predicate<HttpServletRequest>>() {{
+            add(isValidEc2pkcs7Request);
+            add(isValidEc2IdRequest);
+        }});
+
+        final Server server = VaultTestUtils.initHttpMockVault(mockVault);
+        server.start();
+
+        final VaultConfig vaultConfig = new VaultConfig()
+                .address("http://127.0.0.1:8999")
+                .build();
+        final Vault vault = new Vault(vaultConfig);
+
+        final String token1 = vault.auth()
+                .loginByAwsEc2("role","pkcs7",null,null)
+                .getAuthClientToken();
+
+        assertNotNull(token1);
+        assertEquals("c9368254-3f21-aded-8a6f-7c818e81b17a", token1.trim());
+
+        final String token2 = vault.auth()
+                .loginByAwsEc2("role","identity","signature", null, null)
+                .getAuthClientToken();
+
+        assertNotNull(token2);
+        assertEquals("c9368254-3f21-aded-8a6f-7c818e81b17a", token2.trim());
+    }
+
+    @Test
+    public void testLoginByAwsIam() throws Exception {
+        final Predicate<HttpServletRequest> isValidEc2IamRequest = (request) -> {
+            JsonObject requestBody = readRequestBody(request);
+            return requestBody != null && request.getRequestURI().endsWith("/auth/aws/login") &&
+                    requestBody.getString("iam_http_request_method", "") == "POST" &&
+                    requestBody.getString("iam_http_request_url", "") == "url" &&
+                    requestBody.getString("iam_http_request_body", "") == "body" &&
+                    requestBody.getString("iam_http_request_headers", "") == "headers";
+        };
+
+        final AuthRequesValidatingMockVault mockVault =  new AuthRequesValidatingMockVault(new HashSet<Predicate<HttpServletRequest>>() {{
+            add(isValidEc2IamRequest);
+        }});
+
+        final Server server = VaultTestUtils.initHttpMockVault(mockVault);
+        server.start();
+
+        final VaultConfig vaultConfig = new VaultConfig()
+                .address("http://127.0.0.1:8999")
+                .build();
+        final Vault vault = new Vault(vaultConfig);
+
+        final String token = vault.auth()
+                .loginByAwsIam("role","url","body","headers",null)
+                .getAuthClientToken();
+
+        assertNotNull(token);
+        assertEquals("c9368254-3f21-aded-8a6f-7c818e81b17a", token.trim());
+    }
+}
