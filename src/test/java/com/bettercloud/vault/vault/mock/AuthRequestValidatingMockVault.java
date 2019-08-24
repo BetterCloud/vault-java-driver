891047bf7acbf1b5c4c5f0174c70b8006f5146d1
Joseph Petersen <josephp90@gmail.com>
2019-08-24 21:51:33 +0200

    convert field to local

--- src/test/java/com/bettercloud/vault/vault/mock/AuthRequestValidatingMockVault.java
+++ src/test/java/com/bettercloud/vault/vault/mock/AuthRequestValidatingMockVault.java
@@ -10,27 +10,6 @@ import java.util.function.Predicate;
 public class AuthRequestValidatingMockVault extends MockVault {
     private Predicate<HttpServletRequest> validator;
 
-    private final String validResponse = "{\n" +
-            "  \"renewable\": true,\n" +
-            "  \"auth\": {\n" +
-            "    \"lease_duration\": 1800000,\n" +
-            "    \"metadata\": {\n" +
-            "      \"role_tag_max_ttl\": \"0\",\n" +
-            "      \"instance_id\": \"i-de0f1344\",\n" +
-            "      \"ami_id\": \"ami-fce36983\",\n" +
-            "      \"role\": \"dev-role\",\n" +
-            "      \"auth_type\": \"ec2\",\n" +
-            "      \"nonce\": \"5defbf9e-a8f9-3063-bdfc-54b7a42a1f95\"\n" +
-            "    },\n" +
-            "    \"policies\": [\n" +
-            "      \"default\",\n" +
-            "      \"dev\"\n" +
-            "    ],\n" +
-            "    \"accessor\": \"20b89871-e6f2-1160-fb29-31c2f6d4645e\",\n" +
-            "    \"client_token\": \"c9368254-3f21-aded-8a6f-7c818e81b17a\"\n" +
-            "  }\n" +
-            "}";
-
 
     public AuthRequestValidatingMockVault(Predicate<HttpServletRequest> validator) {
         this.validator = validator;
@@ -45,6 +24,26 @@ public class AuthRequestValidatingMockVault extends MockVault {
         baseRequest.setHandled(true);
         if (validator.test(request)) {
             response.setStatus(200);
+            String validResponse = "{\n" +
+                    "  \"renewable\": true,\n" +
+                    "  \"auth\": {\n" +
+                    "    \"lease_duration\": 1800000,\n" +
+                    "    \"metadata\": {\n" +
+                    "      \"role_tag_max_ttl\": \"0\",\n" +
+                    "      \"instance_id\": \"i-de0f1344\",\n" +
+                    "      \"ami_id\": \"ami-fce36983\",\n" +
+                    "      \"role\": \"dev-role\",\n" +
+                    "      \"auth_type\": \"ec2\",\n" +
+                    "      \"nonce\": \"5defbf9e-a8f9-3063-bdfc-54b7a42a1f95\"\n" +
+                    "    },\n" +
+                    "    \"policies\": [\n" +
+                    "      \"default\",\n" +
+                    "      \"dev\"\n" +
+                    "    ],\n" +
+                    "    \"accessor\": \"20b89871-e6f2-1160-fb29-31c2f6d4645e\",\n" +
+                    "    \"client_token\": \"c9368254-3f21-aded-8a6f-7c818e81b17a\"\n" +
+                    "  }\n" +
+                    "}";
             response.getWriter().println(validResponse);
         } else {
             response.setStatus(400);
